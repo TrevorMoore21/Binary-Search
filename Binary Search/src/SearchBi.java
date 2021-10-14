91ce6926a550bc36e3c984bd4031b333b1967a75
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2021-10-14 11:03:04 -0600

    nailed it

--- Binary Search/src/SearchBi.java
+++ Binary Search/src/SearchBi.java
@@ -3,30 +3,41 @@
 public class SearchBi
 	{
 
+		public static void main(String[] args)
+			{
+				int [] fred = { 1, 2, 4, 5, 7, 8, 9};
+				System.out.println(binarySearch(fred, 7));
+				
+				
+				
+			}
+		
+		
+		
+		
 		public static int binarySearch(int[ ] elements, int target)
 			{     
 			        int left = 0;
 			        int right = elements.length - 1;
 			        while (left <= right) 
-			{
+			        	{
 			            	int mid = (left + right) / 2;
 			            	if (target == elements[mid]) 
-			{
+			            		{
 			                	return mid;
 			            		}
 			            	else if (target < elements[mid]) 
-			{
+			            		{
 			                	right = mid - 1;
 			            		} 
-			else 
-			{
+			            	else 
+			            		{
 			                	left = mid + 1;
 			            		}
-			        		}
+			        	}
 			        return -1;
 			    }
 
-		
 		
 		
 	}
